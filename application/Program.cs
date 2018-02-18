ea3159cddc2cad29ce4ac5bb6fed1c784114171e
Hayden Edgmon <howtodointernet@gmail.com>
2018-02-18 16:22:32 -0600

    Initial setup
    Setup C# environment, reteaching myself namespace and syntax. created Scanner class and begun implamenting file reader.

---
+++ application/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+
+namespace application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("toParse.txt");
+            application.Scanner theScanner = new application.Scanner(sr);
+            theScanner.printToken();
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Scanner{
+        StreamReader reader;
+        string thisToken = "It Worked!";
+        public Scanner(StreamReader passedReader){
+            reader = passedReader;
+        }
+        public void printToken(){
+            Console.WriteLine(thisToken);
+        }
+    }
+}
--- test.js
+++
@@ -1,1 +0,0 @@
-var thisThing = 2;
