86bb8a49abdfe312ed96c13aebca3250ddb6f525
HaydenEdgmon <howtodointernet@gmail.com>
2018-02-19 17:02:13 -0600

    begun detecting constants
    created a checkConstant method with switch to detect number and act accordingly

--- application/Program.cs
+++ application/Program.cs
@@ -9,14 +9,11 @@ namespace application
         static void Main(string[] args)
         {
             StreamReader sr = new StreamReader("toParse.txt");
-            application.Scanner theScanner = new application.Scanner(sr);
+            
             while(sr.Peek() != -1){
-                if(sr.Peek() == 32){
-                    sr.Read();
-                }
-                else{
-                    theScanner.printSeperatedBySpace();
-                }
+                application.Scanner theScanner = new application.Scanner(sr);
+                string token = theScanner.detectToken();
+                Console.WriteLine(token);
             }
             sr.Close();
             Console.WriteLine("Hello World!");
@@ -26,6 +23,7 @@ namespace application
     class Scanner{
         StreamReader reader;
         string thisToken = "It Worked!";
+        int currentLength = 0;
         public Scanner(StreamReader passedReader){
             reader = passedReader;
         }
@@ -35,15 +33,109 @@ namespace application
         public void printPeek(){
             Console.WriteLine((char)reader.Read());
         }
-        public void printSeperatedBySpace(){
+        public string detectToken(){
             char[] printString = new char[50];
+            string s = "";
             int i = 0;
-            while(reader.Peek()!=32){
-                printString[i] = (char)reader.Read();
-                i++;
+            while(reader.Peek() == 32){
+                reader.Read();
             }
-            string s = new string(printString);
-            Console.WriteLine(s);
+            while(reader.Peek()!=32){
+                if(reader.Peek() == -1){
+                    break;
+                }
+                if(currentLength != -1){
+                    if(checkConstant()){
+                        printString[i] = (char)reader.Read();
+                        Console.WriteLine("============================================");
+                    }
+                    else{
+                        printString[i] = (char)reader.Read();
+                        currentLength = -1;
+                    }
+                }
+                else{
+                    currentLength = -1;
+                    printString[i] = (char)reader.Read();
+                }
+                i++;
+                s = new string(printString);
+            }
+            return s;
+        }
+
+        public bool checkKeyword(){
+            bool isKeyword = false;
+
+
+            return isKeyword;
+        }
+        public bool checkConstant(){
+            bool isConstant = false;
+            if((char)reader.Peek() =='1'){
+                isConstant = true;
+            }
+            // case((char)reader.peek()){
+            //     case '0':
+            //         if(currentLength < 1){
+            //             Console.WriteLine("@@@@@@@Not a constant@@@@@");
+            //             isConstant = false;
+            //         }
+            //         else{
+            //             currentLength++;
+            //             isConstant = true;
+            //         }
+            //         break;
+            //     case '1':
+            //         currentLength++;
+            //         isConstant = true;
+            //         break;
+            //     case '2':
+            //         currentLength++;
+            //         isConstant = true;
+            //         break;
+            //     case '3':
+            //         currentLength++;
+            //         isConstant = true;
+            //         break;
+            //     case '4':
+            //         currentLength++;
+            //         isConstant = true;
+            //         break;
+            //     case '5':
+            //         currentLength++;
+            //         isConstant = true;
+            //         break;
+            //     case '6':
+            //         currentLength++;
+            //         isConstant = true;
+            //         break;
+            //     case '7':
+            //         currentLength++;
+            //         isConstant = true;
+            //         break;
+            //     case '8':
+            //         currentLength++;
+            //         isConstant = true;
+            //         break;
+            //     case '9':
+            //         currentLength++;
+            //         isConstant = true;
+            //         break;
+            //     case ' ':
+            //         if(currentLength > 6){
+            //             isConstant = true;
+            //         }
+            //         else{
+            //             Console.WriteLine("@@@@@@@Not a constant@@@@@");
+            //             isConstant = false;
+            //         }
+            //     default:
+            //         Console.WriteLine("@@@@@@@Not a constant@@@@@");
+            //         isConstant = false;
+            //         break;
+            // }
+            return isConstant;
         }
     }
 }
