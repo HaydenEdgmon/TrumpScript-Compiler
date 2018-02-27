b215ab7aa037f22f34b84cd21c5b2c7a1cbc4732
HaydenEdgmon <howtodointernet@gmail.com>
2018-02-27 17:05:47 -0600

    refactoring oll the things
    changing to be state based, almost done with constants

--- application/Program.cs
+++ application/Program.cs
@@ -9,11 +9,11 @@ namespace application
         static void Main(string[] args)
         {
             StreamReader sr = new StreamReader("toParse.txt");
-            string token;
+            Token token;
             while(sr.Peek() != -1){
                 application.Scanner theScanner = new application.Scanner(sr);
                 token = theScanner.detectToken();
-                Console.WriteLine(token);
+                Console.WriteLine(token.ToString());
             }
             sr.Close();
             Console.WriteLine("Hello World!");
@@ -21,9 +21,11 @@ namespace application
     }
 
     class Scanner{
+        States state = States.INITIAL_STATE;
         StreamReader reader;
         string thisToken = "It Worked!";
         int currentLength = 0;
+        Token scannedToken;
         public Scanner(StreamReader passedReader){
             reader = passedReader;
         }
@@ -33,7 +35,7 @@ namespace application
         public void printPeek(){
             Console.WriteLine((char)reader.Read());
         }
-        public string detectToken(){
+        public Token detectToken(){
             
             char[] printString = new char[50];
             string s = "";
@@ -41,53 +43,125 @@ namespace application
             while(reader.Peek() == 32){
                 reader.Read();
             }
-            while(reader.Peek() == 13){
+            while(nextIsNewLineChar()){
                 reader.Read();
             }
-            int k = reader.Peek();
-            Console.WriteLine(reader.Peek());
-            if(47 < reader.Peek() && reader.Peek() < 58){
-                //checking if token is a constant
-                s = checkConstant();
-                if(s != "#*ERROR*#"){
-                    Console.WriteLine("+++++++++++++++++++++++");
-                    return s;
-                }
-                else{
-                    s = s + " IS NOT A CONSTANT";
-                    return s;
-                }
+            while((char)reader.Peek() == '#'){
+                //line is a comment
+                processComment();
             }
-            else if(isNextSpecialChar()){
+            switch(state){
+                case States.INITIAL_STATE:
+                    if(47 < reader.Peek() && reader.Peek() < 58){
+                        //checking if token is a constant
+                        s += reader.Read(); 
+                        state = States.CONSTANT_LENGTH_1;
+                    }
+                    break;
+                case States.CONSTANT_LENGTH_1:
+                    if(47 < reader.Peek() && reader.Peek() < 58){
+                        //checking if token is a constant
+                        s += reader.Read(); 
+                        state = States.CONSTANT_LENGTH_2;
+                    }
+                    break;
+                case States.CONSTANT_LENGTH_2:
+                    if(47 < reader.Peek() && reader.Peek() < 58){
+                        //checking if token is a constant
+                        s += reader.Read(); 
+                        state = States.CONSTANT_LENGTH_3;
+                    }
+                    break;
+                case States.CONSTANT_LENGTH_3:
+                    if(47 < reader.Peek() && reader.Peek() < 58){
+                        //checking if token is a constant
+                        s += reader.Read(); 
+                        state = States.CONSTANT_LENGTH_4;
+                    }
+                    break;
+                case States.CONSTANT_LENGTH_4:
+                    if(47 < reader.Peek() && reader.Peek() < 58){
+                        //checking if token is a constant
+                        s += reader.Read(); 
+                        state = States.CONSTANT_LENGTH_5;
+                    }
+                    break;
+                case States.CONSTANT_LENGTH_5:
+                    if(47 < reader.Peek() && reader.Peek() < 58){
+                        //checking if token is a constant
+                        s += reader.Read(); 
+                        state = States.CONSTANT_LENGTH_6;
+                    }
+                    break;
+                case States.CONSTANT_LENGTH_6:
+                    if(47 < reader.Peek() && reader.Peek() < 58){
+                        //checking if token is a constant
+                        s += reader.Read(); 
+                        state = States.CONSTANT;
+                    }
+                    break;
+                case States.CONSTANT:
+                    if(47 < reader.Peek() && reader.Peek() < 58){
+                        //checking if token is a constant
+                        s += reader.Read(); 
+                        state = States.CONSTANT;
+                    }
+                    else if(nextCharIsSpecial()){
+                        scannedToken = new Token(s, 2);
+                        return scannedToken;
+                    }
+                    else if(nextCharIsWhitespace()){
+                        //!!!!!!!!!!!!HERE!!!!!!!!!!!
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+
+
+            int k = reader.Peek();
+            //Console.WriteLine(reader.Peek());
+            // if(47 < reader.Peek() && reader.Peek() < 58){
+            //     //checking if token is a constant
+            //     s = checkConstant();
+            //     if(s != "#*ERROR*#"){
+            //         scannedToken = new Token(s, 1);
+            //         return scannedToken;
+            //     }
+            //     else{
+            //         state = States.CONSTANT_ERROR;
+            //         scannedToken = new Token(state.ToString(), 5);
+            //         return scannedToken;
+            //         //return s;
+            //     }
+            // }
+            if(nextCharIsSpecial()){
                 // checkString if token is a special character
-                Console.WriteLine("The following is a token seperator");
                 s = "" + (char)reader.Read();
-                return s;
+                scannedToken = new Token(s, 4);
+                return scannedToken;
             }
             else if(reader.Peek() == 34){
                 //checking for quoted string
                 s = checkString();
                 if(s != "#*ERROR*#"){
-                    Console.WriteLine("......string......");
-                    return s;
+                    //Console.WriteLine("......string......");
+                    scannedToken = new Token(s, 2);
+                    return scannedToken;
                 }
                 else{
-                    s = s + " IS NOT A CONSTANT";
-                    return s;
+                    state = States.GENERIC_ERROR;
+                    scannedToken = new Token(state.ToString(), 5);
+                    return scannedToken;
                 }
-            }
-            else if((char)reader.Peek() == '#'){
-                //line is a comment
-                Console.WriteLine("kajshbdkajshdkasjhd");
-                processComment();
-                return s;
             }
             else{
                 while(reader.Peek()!=32 ){
                     if(reader.Peek() == -1){
                         break;
                     }
-                    else if(!isNextSpecialChar()){
+                    else if(!nextCharIsSpecial()){
                         printString[i] = (char)reader.Read();
                         i++;
                     }
@@ -95,8 +169,10 @@ namespace application
                         break;
                     }
                 }
-                s = new string(printString);
-                return s;
+                state = States.SOMETHING_ELSE;
+                s = new string(printString, 0, i);
+                scannedToken = new Token(s, 0);
+                return scannedToken;
             }         
         }
 
@@ -156,7 +232,7 @@ namespace application
                         constString = constString + (char)reader.Read();
                     }
                 }
-                else if(isNextSpecialChar()){
+                else if(nextCharIsSpecial()){
                     break;
                 }
                 else{
@@ -170,7 +246,7 @@ namespace application
                 constString = "#*ERROR*#";
             }
             else if(constString == "1000000"){
-                Console.Write("1000000 ");
+               // Console.Write("1000000 ");
                 constString = "#*ERROR*#";
             }
             //check token seperators
@@ -182,7 +258,23 @@ namespace application
             char returnChar = (char)(reader.Read() + 32);
             return returnChar;
         }
-        public bool isNextSpecialChar(){
+        public bool nextCharIsWhitespace(){
+            bool isWhitespace = false;
+            if(reader.Peek() == 32){
+                isWhitespace = true;
+            }
+            if(reader.Peek() == 9){
+                isWhitespace = true;
+            }
+            if(reader.Peek() == 10){
+                isWhitespace = true;
+            }
+            if(reader.Peek() == 13){
+                isWhitespace = true;
+            }
+            return isWhitespace;
+        }
+        public bool nextCharIsSpecial(){
             bool isSpecial = false;
             if(reader.Peek()==44){
                 //ASCII value of ','
@@ -222,7 +314,7 @@ namespace application
             string returnString = "";
             reader.Read();
             while(reader.Peek() != 34){
-                if(isNextSpecialChar()){
+                if(nextCharIsSpecial()){
                     //detected end of token before quotation wasx closed
                     return "#*ERROR*#";
                 }
@@ -242,5 +334,37 @@ namespace application
             }
             return returnBool;
         }
+    }
+    class Token{
+        string lexeme;
+        Type tokenType;
+        public Token(string passedLexeme, int passedType){
+            lexeme = passedLexeme;
+            tokenType = (Type)passedType;
+        }
+        public string Lexeme{
+            get{
+                return lexeme;
+            }
+        }
+        public string TokenType{
+            get{
+                return tokenType.ToString();
+            }
+        }
+        override
+        public string ToString(){
+            return "Lexeme: " + lexeme + " Type: " + tokenType.ToString();
+        }
+    }
+    enum Type {ID, CONSTANT, STRING, KEYWORD, SPECIAL_SYMBOL, ERROR};
+    enum States{
+        INITIAL_STATE,
+        GENERIC_ERROR,
+        //string states
+        STRING_INVALID, STRING_VALID,
+        //Constant states
+        CONSTANT_LENGTH_1, CONSTANT_LENGTH_2, CONSTANT_LENGTH_3, CONSTANT_LENGTH_4, CONSTANT_LENGTH_5, CONSTANT_LENGTH_6, CONSTANT, CONSTANT_ERROR,
+        SOMETHING_ELSE
     }
 }
